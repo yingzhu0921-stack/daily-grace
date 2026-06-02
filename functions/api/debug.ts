e52f3ed1764bf6a9030cab50573e0db7210d9223
young joo Yoo <youngjooyoo@Youngjoo-MaBookPro.local>
2026-06-02 15:01:49 +0900

    Debug: show more of client ID

--- functions/api/debug.ts
+++ functions/api/debug.ts
@@ -4,7 +4,7 @@ export const onRequest: PagesFunction<Env> = async ({ env }) => {
   return Response.json({
     APP_ORIGIN: env.APP_ORIGIN,
     CLIENT_ID_PREVIEW: env.GOOGLE_CLIENT_ID
-      ? env.GOOGLE_CLIENT_ID.slice(0, 12) + '...' + env.GOOGLE_CLIENT_ID.slice(-20)
+      ? env.GOOGLE_CLIENT_ID.slice(0, 20) + '...' + env.GOOGLE_CLIENT_ID.slice(-20)
       : 'NOT SET',
     CLIENT_SECRET_SET: !!env.GOOGLE_CLIENT_SECRET,
     CLIENT_SECRET_LEN: env.GOOGLE_CLIENT_SECRET?.length ?? 0,
